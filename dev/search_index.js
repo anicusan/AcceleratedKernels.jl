918eb0bc90c296c70ab0cc46f3653bb74085243d
Documenter.jl <documenter@juliadocs.github.io>
2024-06-10 18:22:40 +0000

    build based on bfdb46a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AcceleratedKernels","category":"page"},{"location":"#AcceleratedKernels","page":"Home","title":"AcceleratedKernels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AcceleratedKernels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AcceleratedKernels]","category":"page"}]
+}
